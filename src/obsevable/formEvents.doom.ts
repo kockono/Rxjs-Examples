81241d9e9498d9b1655e7d8726e8a57c2d356356
kockono <kockonoop@gmail.com>
2022-11-15 14:06:23 -0600

    formEvents, events

---
+++ src/obsevable/formEvents.doom.ts
@@ -0,0 +1,32 @@
+import { fromEvent } from 'rxjs';
+
+/**
+ * Eventos del DOM
+ */
+
+const source1$ = fromEvent<MouseEvent>(document, 'click');
+const source2$ = fromEvent<KeyboardEvent>(document, 'keyup');
+// No te permite acceder a las propiedades
+// const source2$ = fromEvent(document, 'keyup');
+
+const observer = {
+    next: val => console.log( 'next', val )
+    
+}
+
+// source1$.subscribe( ev => {
+//     console.log( ev.x )
+//     console.log( ev.y )
+// });
+
+// Destrocturado
+source1$.subscribe( ({ x, y}) => {
+    console.log( x )
+    console.log( y )
+    
+});
+
+source2$.subscribe( evento => {
+    console.log( "evento:", evento.key)
+    
+});
